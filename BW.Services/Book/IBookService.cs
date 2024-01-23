f3ff4861afff2c3971be7d09f72f46f93669ff26
Isaiah Schmidt <isiahdschmidt@outlook.com>
2024-01-23 18:32:07 -0500

    GetBooksFromAuthor done and tested

--- BW.Services/Book/IBookService.cs
+++ BW.Services/Book/IBookService.cs
@@ -11,7 +11,7 @@ namespace BW.Services.Book
         Task<BookListItem?> CreateBookAsync(BookCreate book);
         Task<IEnumerable<BookListItem>> GetAllBooksAsync();
         Task<BookDetail?> GetBookByIdAsync(int bookId);
-
+        Task<IEnumerable<BookListItem>> GetBooksFromAuthorAsync(string author);
         Task<bool> AddSubjectToBook(AddSubjectToBook request);
         Task<bool> RemoveSubjectFromBook(AddSubjectToBook request);
     }
